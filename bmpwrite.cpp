42321af4e44435834240bfb1c95afdeb8bc7f5bc
Aigul <imalg95@gmail.com>
2017-10-27 04:05:37 +0900

    gray3x3 inputcolor

--- bmpwrite.cpp
+++ bmpwrite.cpp
@@ -182,7 +182,7 @@ int main(char argc, char* argv[]) {
 	std::ifstream f;
 	char *filename;
 	if (argc > 1) filename = argv[1]; 
-	else filename = "inputgray.txt";
+	else filename = "gray3x3.txt";
 	f = std::ifstream(filename);
 	if (f.fail()) {
 		std::cerr << "could not open file\n";
